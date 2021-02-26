5552e2b156c9783e5a068dbb8a7bc3b178ad7aff
Alandani <rizki.alandani@gmail.com>
2021-02-26 12:03:24 +0700

    bug fix cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -4,21 +4,24 @@ namespace App\Http\Middleware;
 
 class CorsMiddleware {
     public function handle($request, \Closure $next) {
-        if ($request->isMethod('OPTIONS')) {
-            return response()->json('{"method":"OPTIONS"}', 200, [
-                'Access-Control-Allow-Origin' => '*',
-                'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With, APP-KEY'
-            ]);
-        }
-        if($request->header('APP-KEY')) {
-            if($request->header('APP-KEY') == env("APP_KEY")) {
-                $response = $next($request);
-                $response->header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS');
-                $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
-                $response->header('Access-Control-Allow-Origin', '*');
+        $headers = [
+            'Access-Control-Allow-Origin'      => '*',
+            'Access-Control-Allow-Methods'     => 'HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS',
+            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Max-Age'           => '86400',
+            'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With, APP-KEY'
+        ];
+
+        if ($request->isMethod('OPTIONS'))
+            return response()->json('{"method":"OPTIONS"}', 200, $headers);
+
+        $response = $next($request);
+        foreach($headers as $key => $value)
+            $response->header($key, $value);
+        
+        if($request->header('APP-KEY')) 
+            if($request->header('APP-KEY') == env("APP_KEY")) 
                 return $response;
-            }
-        }
 
         return response()->json([
             'status' => "ERROR",
